918c7dc2ca201ce691954298f928a65437fd02ec
Jeremy dubrulle <jerem-du-75020@hotmail.fr>
2023-07-27 10:31:48 +0200

    first commit

---
+++ form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+        <h1>Mon Formulaire PHP</h1>
+        <div>
+            <label for="firstName">firstName :</label><br>
+            <input type="text"><br>
+            <label for="lastName">lastName :</label><br>
+            <input type="text"><br>
+            <label for="email">E-Mail :</label><br>
+            <input type="email"><br>
+            <input type="submit" class="envoyer">
+        </div>
+    </form>
+</body>
+
+</html>
